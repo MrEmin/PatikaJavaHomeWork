8493fdeea50d72ed482babe01a7b3e1926d96e20
MrEmin <emuhammet.ozdemir@gmail.com>
2022-01-06 23:43:36 +0300

    odev

--- DortVeBesinKuvveti/src/Main.java
+++ DortVeBesinKuvveti/src/Main.java
@@ -4,16 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         int n;
-
-        Scanner scan = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
         System.out.print("Sınır sayısını giriniz : ");
-        n = scan.nextInt();
+        n = input.nextInt();
 
-        for (int i = 1, j = 1; i <= n && j <= n; i *= 4, j *= 5){
-            System.out.println("4'ün Kuvveti : " + i);
-            System.out.println("5'in Kuvveti : " + j);
+        System.out.println("4'ün Katları : ");
+        for (int i = 1; i < n; i*=4){
+            System.out.println(i);
         }
 
+        System.out.println("---------------------------------");
+        
+        System.out.println("5'in Katları : ");
+        for (int i = 1; i < n; i*=5){
+            System.out.println(i);
+        }
     }
 }
+.
