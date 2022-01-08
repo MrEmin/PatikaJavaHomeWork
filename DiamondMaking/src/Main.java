c3a574f0d207409dcb10088ad8f2c7f29e641e02
MrEmin <emuhammet.ozdemir@gmail.com>
2022-01-08 15:15:34 +0300

    diamond making with stars

--- DiamondMaking/src/Main.java
+++ DiamondMaking/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
             if (i == n) {
                 for (int l = (n-1); l >= 1; l--) {
-                    for (int m = 1; m <= n-l; m++) {
+                    for (int m = 1; m <= (n-l); m++) {
                         System.out.print(" ");
                     }
                     for (int x = (2 * (l+1)) - 3; x >= 1; x-- ) {
