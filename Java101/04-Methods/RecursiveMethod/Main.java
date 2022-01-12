8253234c414c52892f0e30f201f18915331c7bd3
MrEmin <emuhammet.ozdemir@gmail.com>
2022-01-13 01:01:09 +0300

    update

--- Java101/04-Methods/RecursiveMethod/Main.java
+++ Java101/04-Methods/RecursiveMethod/Main.java
@@ -6,6 +6,9 @@ public class Main {
     //Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın.
     // Her çıkarma işlemi sırasında ekrana son değeri yazdırın. Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin.
     // Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+    //Senaryolar
+    //N Sayısı : 16
+    //Çıktısı : 16 11 6 1 -4 1 6 11 16
 
     static void decrease(int number, int temp) {
         if (number > 0) {
