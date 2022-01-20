6edf739bef34a8751a3506bda8eca4038b9f42bd
MrEmin <emuhammet.ozdemir@gmail.com>
2022-01-20 23:25:46 +0300

    Finding the Matrix Transpose.

---
+++ Java101/06-Arrays/Transpose/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{2, 3, 4}, {5, 6, 4}};
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        for (int[] transposes : transpose) {
+            for (int number : transposes) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+}
